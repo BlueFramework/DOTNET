be0ecee885bd1077d8483224284961226ffb5d86
wabjtam723 <786574043@qq.com>
2018-05-04 22:30:39 +0800

    补充用户实体

--- BlueFramework.User/Models/UserInfo.cs
+++ BlueFramework.User/Models/UserInfo.cs
@@ -9,11 +9,28 @@ namespace BlueFramework.User.Models
     /// <summary>
     /// user infomation and user action
     /// </summary>
-    public class UserInfo:IUser 
+    public class UserInfo : IUser
     {
         public int UserId { get; set; }
 
         public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string TrueName { get; set; }
+
+        public int IsAdmin { get; set; }
+
+        public DateTime CreatTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+        public DateTime LastLoginTime { get; set; }
+
+        /// <summary>
+        /// 用户状态
+        /// </summary>
+        public int State { get; set; }
 
     }
 }
