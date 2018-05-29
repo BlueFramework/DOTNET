aa2703ec689e7b7ba75d72703a3fb3165a177eef
YoungTsui <cdxuyong@qq.com>
2018-05-29 23:23:48 +0800

    CSV READER

---
+++ BlueFramework.Common/Excel/IExcel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+namespace BlueFramework.Common.Excel
+{
+    public interface IExcel
+    {
+        DataSet Read(string filePath);
+
+        void Write(StreamWriter stream);
+    }
+}
---
+++ BlueFramework.Common/Excel/POIExcel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+
+namespace BlueFramework.Common.Excel
+{
+    public class POIExcel:IExcel
+    {
+        public POIExcel()
+        {
+
+        }
+
+        public DataSet Read(string filePath)
+        {
+            return null;
+        }
+
+        public void Write(StreamWriter stream)
+        {
+
+        }
+    }
+}
