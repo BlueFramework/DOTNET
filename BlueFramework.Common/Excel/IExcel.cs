a6f42064abdfda1cdb59b190af17678f6405df66
YoungTsui <cdxuyong@qq.com>
2018-06-24 10:02:47 +0800

    EXCEL读写类

--- BlueFramework.Common/Excel/IExcel.cs
+++ BlueFramework.Common/Excel/IExcel.cs
@@ -7,10 +7,76 @@ using System.Data;
 
 namespace BlueFramework.Common.Excel
 {
+    /// <summary>
+    /// excel read/write
+    /// </summary>
     public interface IExcel
     {
+
+        /// <summary>
+        /// read <see cref="System.Data.DataSet"/> from stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        DataSet Read(Stream stream);
+
+        /// <summary>
+        /// read <see cref="System.Data.DataSet"/> from excel file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
         DataSet Read(string filePath);
 
-        void Write(StreamWriter stream);
+        /// <summary>
+        /// read <see cref="System.Data.DataTable"/> from excel file's sheet that is limit by regin
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="sheetIndex"></param>
+        /// <param name="dataIndex"></param>
+        /// <returns></returns>
+        DataTable Read(string filePath, int sheetIndex, int dataIndex);
+
+        /// <summary>
+        /// write <see cref="System.Data.DataSet"/> to <see cref="System.IO.Stream"/>
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="ds"></param>
+        /// <param name="extendType"><see cref="ExcelExtendType"/></param>
+        void Write(Stream stream, DataSet ds,ExcelExtendType extendType);
+
+        /// <summary>
+        /// write <see cref="System.Data.DataSet"/> to <see cref="System.IO.Stream"/>
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="template"></param>
+        /// <param name="ds"></param>
+        /// <param name="extendType"></param>
+        void Write(Stream stream,TTemplate template,DataSet ds, ExcelExtendType extendType);
+
+        /// <summary>
+        /// write <see cref="System.Data.DataSet"/> to <see cref="System.IO.Stream"/>
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="templatePath"></param>
+        /// <param name="ds"></param>
+        /// <param name="extendType"></param>
+        void Write(Stream stream, string templatePath, DataSet ds, ExcelExtendType extendType);
+
+        /// <summary>
+        /// write <see cref="Stream"/> to excel file
+        /// </summary>
+        /// <param name="outputFilePath"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        bool WriteFile(string outputFilePath, Stream stream);
+
+        /// <summary>
+        /// write <see cref="DataSet"/> to excel file
+        /// </summary>
+        /// <param name="outputFilePath"></param>
+        /// <param name="ds"></param>
+        /// <param name="template"></param>
+        /// <returns></returns>
+        bool WriteFile(string outputFilePath, DataSet ds, TTemplate template);
     }
 }
---
+++ BlueFramework.Common/Excel/TemplateConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BlueFramework.Common.Excel
+{
+    public class TemplateConfig
+    {
+        private string filePath;
+        private TTemplate template;
+
+        public TemplateConfig()
+        {
+            template = new TTemplate();
+            template.Sheets = new List<TSheet>();
+        }
+
+        public void Load(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void Load(DataSet ds)
+        {
+            foreach(DataTable dt in ds.Tables)
+            {
+                TSheet sheet = new TSheet();
+                sheet.Title = dt.TableName;
+                sheet.Name = sheet.Title;
+                sheet.Head = new THead();
+                TRow row = new TRow();
+                sheet.Head.Rows.Add(row);
+                int i = 0;
+                foreach(DataColumn column in dt.Columns)
+                {
+                    TCell cell = new TCell();
+                    cell.RowIndex = 0;
+                    cell.ColumnIndex = i;
+                    cell.Name = column.ColumnName;
+                    cell.Caption = string.IsNullOrEmpty(column.Caption)?column.ColumnName:column.Caption;
+                    row.Cells.Add(cell);
+                    i++;
+                }
+                this.template.Sheets.Add(sheet);
+            }
+        }
+
+        public TTemplate GetTemplate()
+        {
+            return this.template;
+        }
+    }
+}
